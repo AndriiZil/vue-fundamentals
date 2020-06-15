6178e16ad71896dcbe35a865249575d774b0ea4e
Andrii Zilnyk <andrii.zilnyk@wetelo.com>
2020-06-15 22:54:27 +0300

    vuex completer

---
+++ vue-vuex/src/Actions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="btn btn-success" @click="updateCounter(1)">Add</button>
+    <button class="btn btn-danger" @click="updateCounter(-1)">Substract</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      updateCounter(value) {
+        // this.$store.state.counter +=value
+        // this.$store.commit('changeCounter', value)
+        this.$store.dispatch('asyncChangeCounter', {
+          counterValue: value,
+          timeOutDelay: 0
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
